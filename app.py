e80a841c385f6412575f4e36f07301e3f6904dd8
vnallam09 <nallamothuvenkat09@gmail.com>
2025-07-07 19:27:33 -0400

    Update app.py

--- app.py
+++ app.py
@@ -95,6 +95,18 @@ def server(input, output, session):
     @render_widget
     def plotly_scatterplot():
         return px.scatter(data_frame=penguins_df, x='bill_length_mm', y='bill_depth_mm', color='species', hover_name='species', size_max=10, title="Scatter Plot")
+# --------------------------------------------------------
+# Reactive calculations and effects
+# --------------------------------------------------------
+
+# Add a reactive calculation to filter the data
+# By decorating the function with @reactive, we can use the function to filter the data
+# The function will be called whenever an input functions used to generate that output changes.
+# Any output that depends on the reactive function (e.g., filtered_data()) will be updated when the data changes.
+
+    @reactive.calc
+    def filtered_data():
+        return penguins_df
 
 app = App(app_ui, server)
 
